fceb9fd1841f30753f2279ac283727e795272358
nils865 <nils.staas0806@gmail.com>
2022-05-17 12:09:07 +0200

    Draw method can draw assets now

--- ConsoleRenderer/ConsoleRenderer.py
+++ ConsoleRenderer/ConsoleRenderer.py
@@ -1,6 +1,6 @@
 from PIL import Image
 from os import path
-from nilslib import setTTYFgCol
+from nilslib import setTTYFgCol, resetColor
 from sys import stdout
 
 def loadAsset(filename):
@@ -10,10 +10,10 @@ def loadAsset(filename):
     im = Image.open(path.join(imagePath, filename))
     rgb_im = im.convert('RGB')
     
-    for i in range(im.size[0]):
+    for i in range(im.size[1]):
         list.append([])
-        for j in range(im.size[1]):
-            r, g, b = rgb_im.getpixel((i, j))
+        for j in range(im.size[0]):
+            r, g, b = rgb_im.getpixel((j, i))
             list[i].append([r,g,b])
 
     return list
@@ -22,13 +22,16 @@ def draw(game, assets):
     def drawRow(row):
         out = ""
 
-        for x in range(len(game[row])):
-            for i in assets[game[row][x]]:
-                for j in i:
+        for i in range(len(assets[game[0][0]])):
+            for x in game[row]:
+                for j in assets[x][i]:
                     out += setTTYFgCol(j[0], j[1], j[2])
                     out += "██"
-                out += "\n"
+            out += "\n"
+
         return out
 
     for x in range(len(game)):
-        stdout.write(drawRow(x))
+        stdout.write(f"{drawRow(x)}")
+    
+    resetColor()
