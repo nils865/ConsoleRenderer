e51fe389d7c9cfe3d17def896a1e9a0ecce5b058
nils865 <nils.staas0806@gmail.com>
2022-05-17 09:54:13 +0200

    Added asset loader

--- ConsoleRenderer.py
+++ ConsoleRenderer.py
@@ -1,1 +1,17 @@
-print("debug")
+from PIL import Image
+from os import path
+
+def loadAsset(filename):
+    list = []
+    imagePath = "assets"
+
+    im = Image.open(path.join(imagePath, filename))
+    rgb_im = im.convert('RGB')
+    
+    for i in range(im.size[0]):
+        list.append([])
+        for j in range(im.size[1]):
+            r, g, b = rgb_im.getpixel((i, j))
+            list[i].append([r,g,b])
+
+    return list
